76f111bd8b13feb401fbb5adffe34fd845547324
ijurrr <muhamadfathirgiovani@gmail.com>
2026-04-20 15:06:16 +0700

    Praktikum FSM SO

---
+++ Assets/ScroptableObj/BaseStateSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+public abstract class StateSO : ScriptableObject 
+{
+    public abstract void Enter(EnemyController enemy);
+    public abstract void Execute(EnemyController enemy);
+    public abstract void Exit(EnemyController enemy);
+}
